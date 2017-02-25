da078aa30cc7fe8983d4dce57e56c9b1d86b8219
Matthew <spoonerm12@gmail.com>
2017-02-25 00:53:54 -0500

    Fixed copy commands dll post build script

--- cpCommands.bat
+++ cpCommands.bat
@@ -1,2 +1,2 @@
-xcopy /y "Wah!Commands\bin\Debug\Wah!Commands.dll" "Wah!Core\bin\Debug\Wah!Commands.dll"
+start xcopy /y "Wah!Commands\bin\Debug\Wah!Commands.dll" "Wah!Core\bin\Debug\Wah!Commands.dll"
 pause
