8591c4d5e162b55429c1bd622bdaac0ee36270bf
Matthew Spooner <spoonerm12@gmail.com>
2017-03-02 14:17:48 -0500

    Fixed build script error output of 1 causing error

--- cpCommands.bat
+++ cpCommands.bat
@@ -1,1 +1,4 @@
+del "Wah!Core\bin\Debug\Wah!Commands.dll"
 copy "Wah!Commands\bin\Debug\Wah!Commands.dll" "Wah!Core\bin\Debug\Wah!Commands.dll"
+set rce=%errorlevel%
+if not %rce%==1 exit %rce% else exit 0
